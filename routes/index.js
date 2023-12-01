0e82baa77071a36cda8f3c583243ef49d0aae9e7
Anthony Ndungu <anthonymsalami@gmail.com>
2023-12-01 05:35:08 -0800

    added routes, controllers and the server

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+// routes
+import { Express } from 'express';
+import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
+import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController';
+import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
+import { APIError, errorResponse } from '../middlewares/error';
+
+const injectRoutes = (api) => {
+  api.get('/status', AppController.getStatus);
+  api.get('/stats', AppController.getStats);
